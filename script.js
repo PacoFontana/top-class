8bbf55ebeef2ce4962929955f075df0760cc1bcc
PacoFontana <pacopc2020@gmail.com>
2024-04-19 20:07:53 -0300

    1.3

--- script.js
+++ script.js
@@ -8,12 +8,3 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /*CATALOGO*/
-document.addEventListener("DOMContentLoaded", function() {
-    const catalogoContainer = document.querySelector(".catalogo-container");
-  
-    // Agregar eventos de desplazamiento al contenedor del catálogo
-    catalogoContainer.addEventListener("wheel", function(event) {
-      event.preventDefault();
-      catalogoContainer.scrollLeft += event.deltaY;
-    });
-  });
