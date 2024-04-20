34bff7cb25d5a9ce960c33e10fd49e02231838c6
PacoFontana <pacopc2020@gmail.com>
2024-04-20 01:04:15 -0300

    1.6

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-/*CATALOGO*/
+/*Click*/
+$(document).ready(function() {
+    // Cuando se hace clic en un enlace con la clase 'smooth-scroll'
+    $('.smooth-scroll').on('click', function(event) {
+      // Evitar el comportamiento predeterminado de la navegación
+      event.preventDefault();
+  
+      // Obtener el valor del atributo href del enlace
+      var target = $(this).attr('href');
+  
+      // Desplazarse suavemente al elemento objetivo
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
+      }, 1000); // Duración de la animación en milisegundos
+    });
+  });
+  
